d44ff54a064fe1b8d018a9e7fa5d3247ca72e082
ivanmakarov.7ch <ivanmak.7ch@gmail.com>
2026-05-12 22:05:00 +0300

    Database created!

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+// Database import code below
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -35,8 +38,10 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {
@@ -53,8 +58,13 @@ app.get('/categories', async (req, res) => {
 //   res.send('Hello from MegaExpress!');
 // });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 });
 
